a4281fd5529f9d83ab44a83454d4cf79cc44ce55
Paz Barda <paz.barda@intel.com>
2025-06-04 16:02:38 +0300

    new PR detection

--- graph-service/services/data_driver.py
+++ graph-service/services/data_driver.py
@@ -3,10 +3,10 @@ from typing import Callable
 import threading
 import json
 
-class DataProvider:
-    def __init__(self, kafka_bootstrap_servers: str, kafka_topic: str, group_id: str, callback: Callable[[dict], None]):
+class DataDriver:
+    def __init__(self, kafka_bootstrap_servers: str, kafka_topic: str, group_id: str, processor_callback: Callable[[dict], None]):
         self.kafka_topic = kafka_topic
-        self.callback = callback
+        self.processor_callback = processor_callback
         self.consumer = Consumer({
             'bootstrap.servers': kafka_bootstrap_servers,
             'group.id': group_id,
@@ -24,7 +24,7 @@ class DataProvider:
                 if msg.error():
                     print(f"Consumer error: {msg.error()}")
                     continue
-                self.callback(msg.value())
+                self.processor_callback(msg.value())
         except KeyboardInterrupt:
             print("Consumer shutting down", flush=True)
         finally:
--- graph-service/services/processor.py
+++ graph-service/services/processor.py
@@ -1,22 +1,50 @@
 import networkx as nx
 from common.models.pr_event import PREventDto
+from services.publisher import Publisher
 
 class Processor:
     def __init__(self):
         self.graph = nx.DiGraph()
-
+        self.new_pr_event_publisher = Publisher(kafka_bootstrap_servers="kafka:9092", kafka_topic="new_PR_events")
+    
     def process(self, json_string: str):
         try:
             pr_event = PREventDto.from_flat_json(json_string)
             if pr_event is None:
                 print("No PR event to process", flush = True)
                 return
-            if pr_event.action == "opened":
-                self.graph.add_edge(pr_event.pr_user, pr_event.pr_number, label="opened_pr")
-                self.graph.add_edge(pr_event.pr_number, pr_event.repo_name, label="related_to_repo")
-                for file in pr_event.pr_files_changed:
-                    self.graph.add_edge(pr_event.pr_number, file, label="modified_file")
-                    self.graph.add_edge(pr_event.repo_name, file, label="contains_file")
+            self.__handle_pr_event(pr_event)
+        except ValueError as e:
+            print(f"Invalid JSON format: {e}", flush = True)
+            return
         except Exception as e:
             print(f"Error processing pr_event: {e}", flush = True)
             return
+    
+    def __handle_pr_event(self, pr_event: PREventDto):
+        if pr_event.pr_number not in self.graph.nodes:
+            self.__handle_new_pr(pr_event)
+        if pr_event.action == "opened":
+                self.__handle_pr_opened_event(pr_event)
+        elif pr_event.action == "submitted":
+            if pr_event.review_state == "commented":
+                self.__handle_pr_comment_event(pr_event)
+            elif pr_event.review_state == "approved":
+                self.__handle_pr_approved_event(pr_event)
+        
+    def __handle_pr_opened_event(self, pr_event: PREventDto):
+        self.graph.add_edge(pr_event.pr_user, pr_event.pr_number, label="opened_pr")
+        self.graph.add_edge(pr_event.pr_number, pr_event.repo_name, label="related_to_repo")
+        for file in pr_event.pr_files_changed:
+            self.graph.add_edge(pr_event.pr_number, file, label="modified_file")
+            self.graph.add_edge(pr_event.repo_name, file, label="contains_file")
+
+    def __handle_pr_comment_event(self, pr_event: PREventDto):
+        self.graph.add_edge(pr_event.pr_user, pr_event.pr_number, label="commented_on_pr")
+
+    def __handle_pr_approved_event(self, pr_event: PREventDto):
+        self.graph.add_edge(pr_event.pr_user, pr_event.pr_number, label="approved_pr")
+
+    def __handle_new_pr(self, pr_event: PREventDto):
+        self.new_pr_event_publisher.publish(pr_event)
+
