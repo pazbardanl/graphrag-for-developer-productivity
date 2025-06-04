5d9389fa402f9fb7ceb5b3998d04823d2db3cfc5
Paz Barda <paz.barda@intel.com>
2025-06-04 16:20:19 +0300

    Refactor: improved DI, extract configs to shared env vars

--- ingestion-service/main.py
+++ ingestion-service/main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import time
 from services.data_provider import DataProvider
@@ -7,15 +8,18 @@ from common.models.pr_event import PREventDto
 
 def main():
     print("Ingestion service started")
-    dataProvider = DataProvider("mock_data/mock_github_events.json")
+    mock_data_path = os.environ.get("MOCK_DATA_PATH", "mock_data/mock_github_events.json")
+    kafka_bootstrap_servers = os.environ["KAFKA_BOOTSTRAP_SERVERS"]
+    pr_events_topic = os.environ["KAFKA_PR_EVENTS_TOPIC"]
+
+    dataProvider = DataProvider(mock_data_path)
     processor = Processor()
-    publisher = Publisher(kafka_bootstrap_servers="kafka:9092", kafka_topic="PR_events")
+    publisher = Publisher(kafka_bootstrap_servers=kafka_bootstrap_servers, kafka_topic=pr_events_topic)
     json_string = dataProvider.provide()
     events = processor.process(json_string)
     for event in events:
         publisher.publish(event=event)
     print("Ingestion service exited")
-
 
 if __name__ == "__main__":
     main()
